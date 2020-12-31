ad12c199a4c31f9ca66734d60cdfc2d0b5e521d5
Kumar <SkKumar@ad.harman.com>
2020-12-31 23:15:47 +0530

    Third lecture

---
+++ flaskblog.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '83c0c8bfc83c7b1bd0abfb1c93185e3b'
+
+posts = [
+    {
+        'author' : 'Swaroop Kumar',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2020'
+    },
+    {
+        'author': 'Jane Patrick',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2020'
+    },
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts = posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title = 'About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title = 'Register', form = form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form = form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
